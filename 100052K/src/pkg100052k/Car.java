52d5ec925aa4db46e0f1474b268cc8fb06ed78d0
Chamika Bandara <kasun.chamika@gmail.com>
2012-01-18 21:12:41 +0530

    my first GIT project

---
+++ 100052K/src/pkg100052k/Car.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg100052k;
+
+
+public class Car {
+
+    
+    protected String name;
+    protected String type;
+    protected String colour;
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public String getColour(){
+        return colour;
+    }
+   
+    
+}
---
+++ 100052K/src/pkg100052k/CarRase.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg100052k;
+
+
+
+
+public class CarRase {
+
+    public CarRase() {
+        
+        RacingCar one = new RacingCar();
+        RacingCar two = new RacingCar();
+        RacingCar three = new RacingCar();        
+        
+    }
+  
+ 
+    
+    public static void main(String[] args) {
+        
+        CarRase race = new CarRase();
+        
+    }
+}
